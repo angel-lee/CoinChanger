85fcd40b36bcc4bb89d0e339c1053ca51575ded5
Angel Lee <angel.lee2011@gmail.com>
2017-11-25 12:22:37 -0500

    Fourth branch commit

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,10 +1,10 @@
 class Changer 
-	def change(amount)
-		return_amount = []
-		[25, 10, 5, 1].each do |coin|
-			return_amount << [coin] * (amount / coin) if amount >= coin
-			amount = amount - (coin * (amount / coin))
-		end
-		return_amount.flatten
-	end
+    def change(amount)
+        return_amount = []
+        [25, 10, 5, 1].each do |coin|
+            return_amount << [coin] * (amount / coin) if amount >= coin
+            amount = amount - (coin * (amount / coin))
+        end
+        return_amount.flatten
+    end
 end 
--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -1,48 +1,49 @@
 require 'changer'
 
 describe Changer do
-	let(:changer) {Changer.new}
-	it 'has a change method' do
-		expect(changer.respond_to?(:change)).to eq(true) 
-	end
+    let (:changer) {Changer.new}
 
-	it 'returns 1 penny for 1 penny' do
-		expect(changer.change(1)).to eq([1])
-	end
+    it 'has a change method' do
+        expect(changer.respond_to?(:change)).to eq(true)
+    end
 
-	it 'returns 3 pennies for 3' do
-		expect(changer.change(3)).to eq([1, 1, 1])
-	end
+    it 'returns 1 penny for 1 penny' do
+        expect(changer.change(1)).to eq([1])
+    end
 
-	it 'returns 1 nickel for 5' do
-		expect(changer.change(5)).to eql([5])
-	end
+    it 'returns 3 pennies for 3' do
+        expect(changer.change(3)).to eq([1, 1, 1])
+    end
 
-	it 'returns 1 dime for 10' do
-		expect(changer.change(10)).to eql([10])
-	end 
+    it 'returns 1 nickel for 5' do
+        expect(changer.change(5)).to eq([5])
+    end
 
-	it 'returns 2 dimes for 20' do
-		expect(changer.change(20)).to eql([10, 10])
-	end 
+    it 'returns 1 dime for 10' do
+        expect(changer.change(10)).to eq([10])
+    end
 
-	it 'returns 1 quarter for 25' do
-		expect(changer.change(25)).to eql([25])
-	end 
+    it 'returns 2 dimes for 20' do
+        expect(changer.change(20)).to eq([10, 10])
+    end
 
-	it 'returns 2 quarters for 50' do
-		expect(changer.change(50)).to eql([25, 25])
-	end 
+    it 'returns 1 quarter for 25' do
+        expect(changer.change(25)).to eq([25])
+    end
 
-	it 'returns 3 quarters for 75' do
-		expect(changer.change(75)).to eql([25, 25, 25])
-	end 
+    it 'returns 2 quarters for 50' do
+        expect(changer.change(50)).to eq([25, 25])
+    end
 
-	it 'returns 2 dimes and 4 pennies for 24' do
-		expect(changer.change(24)).to eql([10, 10, 1, 1, 1, 1])
-	end
+    it 'returns 3 quarters for 75' do
+        expect(changer.change(75)).to eq([25, 25, 25])
+    end
 
-	it 'returns 3 quarters and 2 dimes and 4 pennies for 99' do
-		expect(changer.change(99)).to eql([25, 25, 25, 10, 10, 1, 1, 1, 1])
-	end
+    it 'returns 2 dimes and 4 pennies for 24' do
+        expect(changer.change(24)).to eq([10, 10, 1, 1, 1, 1])
+    end
+
+    it 'returns 3 quarters and 2 dimes and 4 pennies for 99' do
+        expect(changer.change(99)).to eq([25, 25, 25, 10, 10, 1, 1, 1, 1])
+    end
 end
